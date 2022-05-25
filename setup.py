0fa78e22424a77918a33b28afe475fb2263e8370
Udayrajsinh Jadeja <ujadeja86@gmail.com>
2022-05-25 13:12:33 +0530

    initial outline

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'cv_image_suit',
+  packages = find_packages(),
+  include_package_data=True,
+  version = '0.1',
+  license='GNU',
+  description = 'Its an auto image classification library',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/uday446/cv_image_suit",
+  author = 'Udayrajsinh Jadeja',
+  author_email = 'ujadeja96@gmail.com',
+  keywords = ['cv_image_suit'],
+  install_requires=[
+        'tensorflow==2.4.1',
+        'scipy',
+        'numpy',
+        'pandas',
+        'Pillow==8.3.2',
+        'Flask',
+        'Flask-Cors==3.0.10'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: GNU License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  entry_points={
+        "console_scripts": [
+            "cv_image_suit = ImageSeeker.main:start_app",
+        ]},
+)
