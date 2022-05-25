00b5518c4f3965c206467879929a751d25bddcb3
Udayrajsinh Jadeja <ujadeja86@gmail.com>
2022-05-25 13:09:38 +0530

    initial outline

---
+++ cv_image_suit/predict.py
@@ -0,0 +1,43 @@
+
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+class dogcat:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    def predictiondogcat(self):
+        from cv_image_suit.utils import data_manager as dm
+        from cv_image_suit.utils.config import configureModel
+        from cv_image_suit.utils.config import configureData
+
+        config_data = configureData()
+        config_model = configureModel()
+
+        # load model
+        model_path = f"New_trained_model/{'new' + config_model['MODEL_NAME'] + '.h5'}"
+        print('Loading...', model_path)
+        model = load_model(model_path)
+
+        # summarize model
+        #model.summary()
+        imagename = self.filename
+        predict = dm.manage_input_data(imagename)
+        result = model.predict(predict)
+        results = np.argmax(result, axis=-1)
+        print(dm.class_name())
+        print(results)
+        out = str(results[0])
+
+        return [{ "image_class" : out}]
+
+        # if results[0] == 1:
+        #     prediction = 'dog'
+        #     return [{ "image" : prediction}]
+        # else:
+        #     prediction = 'cat'
+        #     return [{ "image" : prediction}]
+
+
