b7d2bbb8c154b6376a46e097d1cc4ed07781acbb
Udayrajsinh Jadeja <ujadeja86@gmail.com>
2022-06-05 11:53:38 +0530

    making deployment

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     'Topic :: Software Development :: Build Tools',
 
     # Pick your license as you wish
-    'License :: OSI Approved :: GNU License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
